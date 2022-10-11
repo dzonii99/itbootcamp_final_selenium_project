b183e205554cacbdf19f19d236d41dbcebf1db24
Nikola Milovanovic <nikolla.millovanovic@gmail.com>
2022-10-12 01:06:25 +0200

    Init Login 2nd Test Completed

--- src/tests/LoginTests.java
+++ src/tests/LoginTests.java
@@ -18,42 +18,4 @@ public class LoginTests extends BasicTest {
 		Assert.assertTrue(signUpPage.getEmailInput().getAttribute("type").equals("email"));
 		Assert.assertTrue(signUpPage.getPasswordInput().getAttribute("type").equals("password"));
 	}
-
-	@Test(priority = 30)
-	public void displaysErrorsWhenUserDoesNotExist() {
-		navPage.getLoginNavButton().click();
-		loginPage.getEmailInput().sendKeys("non-existing-user@gmal.com");
-		loginPage.getPasswordInput().sendKeys("password123");
-		loginPage.getLoginButton().click();
-		messagePopUpPage.waitMessageToBeVisible();
-		Assert.assertTrue(messagePopUpPage.getTextMessage().getText().equals("User does not exists"));
-		Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
-	}
-
-	@Test(priority = 40)
-	public void displaysErrorsWhenPasswordIsWrong() {
-		navPage.getLoginNavButton().click();
-		loginPage.getEmailInput().sendKeys("admin@admin.com");
-		loginPage.getPasswordInput().sendKeys("password123");
-		loginPage.getLoginButton().click();
-		messagePopUpPage.waitMessageToBeVisible();
-		Assert.assertTrue(messagePopUpPage.getTextMessage().getText().equals("Wrong password"));
-		Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
-	}
-
-	@Test(priority = 50)
-	public void login() throws InterruptedException {
-		navPage.getLoginNavButton().click();
-		loginPage.getEmailInput().sendKeys("admin@admin.com");
-		loginPage.getPasswordInput().sendKeys("12345");
-		loginPage.getLoginButton().click();
-		Thread.sleep(1000);
-		Assert.assertTrue(driver.getCurrentUrl().contains("/home"));
-	}
-
-	@Test(priority = 60)
-	public void logout() {
-		Assert.assertTrue(navPage.getLogoutButton().isDisplayed());
-		navPage.getLogoutButton().click();
-	}
 }
