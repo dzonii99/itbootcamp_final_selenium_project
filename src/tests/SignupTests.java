ed940bf639b8bf0813e8ac63e4903f4995d834ae
Nikola Milovanovic <nikolla.millovanovic@gmail.com>
2022-10-12 01:25:54 +0200

    Init second Signup Test

--- src/tests/SignupTests.java
+++ src/tests/SignupTests.java
@@ -10,4 +10,13 @@ public class SignupTests extends BasicTest {
 		navPage.getSignUpButton().click();
 		Assert.assertTrue(driver.getCurrentUrl().contains("/signup"));
 	}
+
+	@Test
+	public void checksInputTypes() {
+		navPage.getSignUpButton().click();
+		Assert.assertTrue(signUpPage.getNameInput().getAttribute("type").equals("text"));
+		Assert.assertTrue(signUpPage.getEmailInput().getAttribute("type").equals("email"));
+		Assert.assertTrue(signUpPage.getPasswordInput().getAttribute("type").equals("password"));
+		Assert.assertTrue(signUpPage.getConfirmPasswordInput().getAttribute("type").equals("password"));
+	}
 }
